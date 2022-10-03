63ed2c7049eb0dda51a18fe90978f84ee92d78cf
Urok1 <a13-78@mail.ru>
2022-10-03 18:35:43 +0400

    Выполненное домашнее задание

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,8 +1,5 @@
 public class Bicycle extends Transport {
 
-    public String modelName;
-    public int wheelsCount;
-
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
